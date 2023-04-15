bd4dbac06b74e46a91d1f464a91f46567b5d7abb
Eric Leo <120332136+ELeo10@users.noreply.github.com>
2023-04-14 22:00:10 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,25 +1,23 @@
 function displayDetails() {
-    var txt1 = document.getElementById('nome').value;
-    var ende1 = document.getElementById('endereço').value;
-    var year1 = document.getElementById('ano').value;
-
+    const txt1 = document.getElementById('nome').value;
+    const ende1 = document.getElementById('endereço').value;
+    const year1 = document.getElementById('ano').value;
+    const output = document.getElementById('output1');
+    const divContainer = document.querySelector('#output1');
+    const divLoginBox = document.querySelector('.login-box');
+    
     var date = new Date();
     var year2 = date.getFullYear();
 
     var year = year2 - year1;
-    
-    if(!txt1 || !ende1 || !year1) {
-        alert("Por favor, preencha todos os campos");
-        return;
+
+    if (!txt1 || !year1 || !ende1) {
+        window.alert("Preencha todos os campos");
+    } else if (year < 18) {
+        window.alert("Você precisa ter acima de 18 anos de idade.")
+    } else {
+        output.innerHTML = 'Seja bem-vinde, ' +txt1+ '. Você tem ' +year+ ' anos de idade, e seu endereço é ' +ende1+ '.';
+        divContainer.style.display = 'inline-block';
+        divLoginBox.style.height = '500px';
     }
-
-    document.getElementById('output1').innerHTML = 'Seja bem-vinde, ' +txt1+ '. Você tem ' +year+ ' anos de idade, e seu endereço é ' +ende1+ '.';
-}
-
-const divContainer = document.querySelector('#output1');
-const divLoginBox = document.querySelector('.login-box');
-
-let divDisplay = function() {
-    divContainer.style.display = 'inline-block';
-    divLoginBox.style.height = '500px';
 }
